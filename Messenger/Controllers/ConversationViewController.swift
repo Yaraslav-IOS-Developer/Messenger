aa6c89d14f3ceb373fd259681a6361b3195cd3bb
Yaroslav <Yaraslav.IOS.Developer@gmail.com>
2021-05-25 19:00:21 +0300

    Building user interface

--- Messenger/Controllers/ConversationViewController.swift
+++ Messenger/Controllers/ConversationViewController.swift
@@ -7,19 +7,58 @@
 
 import UIKit
 import FirebaseAuth
+import JGProgressHUD
 
 class ConversationViewController: UIViewController {
+    
+    private let spiner = JGProgressHUD(style: .dark)
+    
+    private let tableView: UITableView = {
+       let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let noConversationsLabel: UILabel = {
+       let label = UILabel()
+        label.text = "No Conversations!"
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.isHidden = true
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose,
+                                                            target: self,
+                                                            action: #selector(didTapComposeButton))
+        
+        setupTableView()
+        fetchConversation()
+        
+        view.addSubview(tableView)
+        view.addSubview(noConversationsLabel)
+    }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
         validateAuth()
+    }
+    
+    @objc private func didTapComposeButton() {
+        let vc = NewConversationViewController()
+        let navVC = UINavigationController(rootViewController: vc)
+        present(navVC, animated: true)
+        
     }
     private func validateAuth() {
         if FirebaseAuth.Auth.auth().currentUser == nil {
@@ -32,5 +71,41 @@ class ConversationViewController: UIViewController {
         }
     }
 
+    // MARK: - Private methods
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func fetchConversation() {
+        tableView.isHidden = false
+    }
+    
+
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension ConversationViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let  cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = ChatViewController()
+        vc.title = "jenny Smith"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
 
--- Messenger/Controllers/Login/RegisterViewController.swift
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 import FirebaseAuth
+import JGProgressHUD
 
 class RegisterViewController: UIViewController {
+    
+    private let spinner = JGProgressHUD(style: .dark)
     
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
@@ -194,10 +197,15 @@ class RegisterViewController: UIViewController {
             alertUserLoginError()
             return
         }
-        
+        spinner.show(in: view)
         // Firebase Log in
         DatabaseManager.shared.userExists(with: email) {[weak self] (exists) in
             guard let strongSelf = self else { return }
+            
+            DispatchQueue.main.async {
+                strongSelf.spinner.dismiss()
+            }
+            
             guard !exists else {
                 // user already exists
                 strongSelf.alertUserLoginError(message: "Looks like a user account for that email address already exists.")
